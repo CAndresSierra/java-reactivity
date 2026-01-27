c8abe8bfe057df2b27b798fc41c80e6e0386d9a1
CAndresSierra <camilofullstack@gmail.com>
2026-01-26 20:23:34 -0500

    feat(reactive): implement flow combination operators and enhance observer example
    - Add Student class with name, age fields and standard methods
    - Implement Merge class demonstrating merge, concat, and zip operators with RxJava
    - Refactor ObserverExample01 to use Observer interface with disposable logic and multiple observers

--- src/main/java/com/camilo/proyect/flowCombination/Merge.java
+++ src/main/java/com/camilo/proyect/flowCombination/Merge.java
@@ -1,4 +1,36 @@
 package com.camilo.proyect.flowCombination;
 
+import io.reactivex.rxjava3.core.Observable;
+
+import java.util.concurrent.TimeUnit;
+
 public class Merge {
+    public static void main(String[] args) throws InterruptedException {
+//        Observable<String> students = Observable.just("Mario", "Ana", "Camilo", "Ancizar")
+//                .delay(1, TimeUnit.SECONDS);
+//
+//        Observable<String> teachers = Observable.just("Prof.Carlos", "Prof.Ana", "Prof.Camilo", "Prof.Ricardo");
+//
+//        Observable.merge(students, teachers).subscribe(System.out::println);
+//
+//        Thread.sleep(2000);
+
+//        Observable<String> students = Observable.just("Mario", "Ana", "Camilo", "Ancizar")
+//                .delay(1, TimeUnit.SECONDS);
+//
+//        Observable<String> teachers = Observable.just("Prof.Carlos", "Prof.Ana", "Prof.Camilo", "Prof.Ricardo");
+//
+//        Observable.concat(students, teachers).subscribe(System.out::println);
+//
+//        Thread.sleep(2000);
+
+
+        Observable<String> students = Observable.just("Mario", "Ana", "Camilo", "Ancizar");
+        Observable<Integer> ages = Observable.just(20, 30, 40, 50);
+
+        Observable.zip(students, ages, Student::new)
+                .subscribe(System.out::println);
+
+
+    }
 }
--- src/main/java/com/camilo/proyect/observer/ObserverExample01.java
+++ src/main/java/com/camilo/proyect/observer/ObserverExample01.java
@@ -1,24 +1,73 @@
 package com.camilo.proyect.observer;
 
+import io.reactivex.rxjava3.annotations.NonNull;
 import io.reactivex.rxjava3.core.Observable;
+import io.reactivex.rxjava3.core.Observer;
+import io.reactivex.rxjava3.disposables.Disposable;
 
 import java.util.Arrays;
-import java.util.List;
+import java.util.Objects;
 
 public class ObserverExample01 {
     public static void main(String[] args) {
         Observable<String> courseStream = Observable.fromIterable(
                 Arrays.asList("HTML", "CSS", null , "C")
                         .stream()
-                        .filter(course -> course != null).toList()
+                        .filter(Objects::nonNull).toList()
         );
 
-        courseStream
-                .subscribe(
-                item -> System.out.println("Received: " + item),
-                error -> System.out.println(error.getMessage()),
-                () -> System.out.println("END")
-        );
+        Observer<String> observer = new Observer<>() {
+            private Disposable disposable;
+            @Override
+            public void onSubscribe(@NonNull Disposable d) {
+                this.disposable = d;
+                System.out.println("Im subscribe!!!!");
+            }
+
+            @Override
+            public void onNext(@NonNull String s) {
+                if(s.equalsIgnoreCase("c")) {
+                    disposable.dispose();
+                } else {
+                    System.out.println("Received: " + s);
+                }
+            }
+
+            @Override
+            public void onError(@NonNull Throwable e) {
+                System.out.println("Error: " + e.getMessage());
+            }
+
+            @Override
+            public void onComplete() {
+                System.out.println("END...");
+            }
+        };
+
+        Observer<String> observer2 = new Observer<>() {
+            @Override
+            public void onSubscribe(@NonNull Disposable d) {
+                System.out.println("Im subscribe, im observer two!!!!");
+            }
+
+            @Override
+            public void onNext(@NonNull String s) {
+                System.out.println("Im observer two and Received: " + s);
+            }
+
+            @Override
+            public void onError(@NonNull Throwable e) {
+                System.out.println("Error: " + e.getMessage());
+            }
+
+            @Override
+            public void onComplete() {
+                System.out.println("END observer two...");
+            }
+        };
+
+        courseStream.subscribe(observer);
+        courseStream.subscribe(observer2);
 
 
     }
